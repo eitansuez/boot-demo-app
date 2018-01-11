8e62fd0cdb937f577d9120c19456bccc68208e50
Eitan Suez <eitan.suez@gmail.com>
2018-01-10 22:28:09 -0600

    added jackson marshalling support for java time module

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 @EnableConfigurationProperties(HelloProperties.class)
@@ -17,4 +22,14 @@ public class DemoApplication {
   public TodoRepository todoRepository() {
     return new InMemoryTodoRepository();
   }
+
+  @Bean
+  public ObjectMapper jsonObjectMapper() {
+    return Jackson2ObjectMapperBuilder.json()
+        .serializationInclusion(JsonInclude.Include.NON_NULL) // Don’t include null values
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
+        .modules(new JavaTimeModule())
+        .build();
+  }
+
 }
