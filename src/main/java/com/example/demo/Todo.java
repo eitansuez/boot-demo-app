d04de85ad5c8cddaea047d7a13b13f40936d05c7
Eitan Suez <eitan.suez@gmail.com>
2018-01-11 15:21:27 -0600

    added spring data jpa dependency, h2, java8 hibernate support, marked Todo an entity, and added the jpa repository interface for todos

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 @Builder(toBuilder = true)
 @Data
 public class Todo {
+  @Id @GeneratedValue
   private final Long id;
   private final String title, description;
   private final LocalDate dueDate;
